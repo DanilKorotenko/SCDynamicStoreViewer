a21f7308086ea8ef590e856f8e951a151c64a379
Danil Korotenko <danil.korotenko@gmail.com>
2021-10-27 16:26:28 +0300

    Initial Commit

---
+++ SCDynamicStoreViewer/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SCDynamicStoreViewer
+//
+//  Created by Danil Korotenko on 10/27/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
