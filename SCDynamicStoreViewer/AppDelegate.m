529374ef1fbde81f71806f6fac60df37ad63f0fe
Danil Korotenko <danil.korotenko@gmail.com>
2021-10-28 13:11:51 +0300

    Implemented hierarchy view of dynamic store keys.

--- SCDynamicStoreViewer/AppDelegate.m
+++ SCDynamicStoreViewer/AppDelegate.m
@@ -6,27 +6,153 @@
 //
 
 #import "AppDelegate.h"
+#import <SystemConfiguration/SystemConfiguration.h>
 
 @interface AppDelegate ()
 
 @property (strong) IBOutlet NSWindow *window;
+@property (strong) IBOutlet NSOutlineView *outlineView;
+
 @end
 
 @implementation AppDelegate
-
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+{
+    NSMutableDictionary *_outlineViewItems;
 }
 
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    [self refreshItems];
+    [self.outlineView reloadData];
+}
 
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
+- (void)applicationWillTerminate:(NSNotification *)aNotification
+{
     // Insert code here to tear down your application
 }
 
-
-- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app
+{
     return YES;
 }
 
+#pragma mark Outline view data source
+
+- (NSInteger)outlineView:(NSOutlineView *)outlineView numberOfChildrenOfItem:(nullable id)item
+{
+    NSDictionary *currentItem = nil;
+
+    if ([item isKindOfClass:[NSString class]])
+    {
+        currentItem = [_outlineViewItems objectForKey:item];
+    }
+    else
+    {
+        currentItem = (item == nil) ? _outlineViewItems : (NSDictionary*)item;
+    }
+
+    return [[currentItem[@"children"] allKeys] count];
+}
+
+- (id)outlineView:(NSOutlineView *)outlineView child:(NSInteger)index ofItem:(nullable id)item
+{
+    NSDictionary *currentItem = nil;
+
+    if ([item isKindOfClass:[NSString class]])
+    {
+        currentItem = [_outlineViewItems objectForKey:item];
+    }
+    else
+    {
+        currentItem = (item == nil) ? _outlineViewItems : (NSDictionary*)item;
+    }
+
+    NSString *itemKey = [[currentItem[@"children"] allKeys] objectAtIndex:index];
+    return [currentItem[@"children"] objectForKey:itemKey];
+}
+
+- (BOOL)outlineView:(NSOutlineView *)outlineView isItemExpandable:(id)item
+{
+    NSDictionary *currentItem = nil;
+
+    if ([item isKindOfClass:[NSString class]])
+    {
+        currentItem = [_outlineViewItems objectForKey:item];
+    }
+    else
+    {
+        currentItem = (item == nil) ? _outlineViewItems : (NSDictionary*)item;
+    }
+
+    return [currentItem[@"children"] count] > 0;
+}
+
+- (nullable id)outlineView:(NSOutlineView *)outlineView
+    objectValueForTableColumn:(nullable NSTableColumn *)tableColumn byItem:(nullable id)item
+{
+//    if ([item isKindOfClass:[NSString class]])
+//    {
+//        return [_outlineViewItems objectForKey:item];
+//    }
+
+    NSDictionary *currentItem = (item == nil) ? _outlineViewItems : (NSDictionary*)item;
+
+    return [currentItem objectForKey:@"title"];
+}
+
+
+#pragma mark -
+
+- (void)refreshItems
+{
+    if (_outlineViewItems == nil)
+    {
+        _outlineViewItems = [NSMutableDictionary dictionary];
+        _outlineViewItems[@"children"] = [NSMutableDictionary dictionary];
+    }
+
+    CFArrayRef keysRef = SCDynamicStoreCopyKeyList(NULL, CFSTR(".*"));
+
+    NSArray *keys = CFBridgingRelease(keysRef);
+
+//    NSLog(@"%@", keys);
+
+    NSCharacterSet *charSet = [NSCharacterSet characterSetWithCharactersInString:@":/"];
+
+    NSMutableDictionary *currentDictionary = _outlineViewItems;
+
+    for (NSString *item in keys)
+    {
+        currentDictionary = _outlineViewItems;
+        NSArray *components = [item componentsSeparatedByCharactersInSet:charSet];
+        for (NSString *component in components)
+        {
+            if (component.length == 0)
+            {
+                continue;
+            }
+            NSMutableDictionary *currentItem = currentDictionary[@"children"][component];
+            if (currentItem == nil )
+            {
+                NSMutableDictionary *newItem = [NSMutableDictionary dictionary];
+                if ([[components lastObject] isEqualTo:component]) // it is a last item
+                {
+                    newItem[@"fullPath"] = item;
+                }
+                newItem[@"title"] = component;
+                newItem[@"children"] = [NSMutableDictionary dictionary];
+
+                currentDictionary[@"children"][component] = newItem;
+
+                currentDictionary = newItem;
+            }
+            else
+            {
+                currentDictionary = currentItem;
+            }
+        }
+    }
+
+}
 
 @end
